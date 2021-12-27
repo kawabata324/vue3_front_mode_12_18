6f899fafe2d704ca597790bf537e332e7d3fe7b9
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-27 15:52:11 +0900

    ログイン画面の編集

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home"></div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "../components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent } from "@vue/runtime-core";
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  setup() {
+    return {};
   },
-})
-export default class Home extends Vue {}
+});
 </script>
---
+++ src/views/RecruitmentExam.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core"
+
+export default defineComponent({
+  setup() {
+    return{
+
+    }
+  },
+})
+</script>
+
+
+<style>
+
+</style>
